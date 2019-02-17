1fa88987a6aeeb1cc76b3fb1217c86d54aad7f13
Daniel Cortes <d5t5c5@yahoo.com>
2019-02-16 22:40:01 -0600

    feat: Set up links in Navbar and added Bootsrtap

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function navbar() {
   return (
-    <div>
-      This is the Navbar
-    </div>
+    <nav>
+      <div className="container">
+        <div className="navbar-nav">
+          <Link className="navbar-brand" to="/">Home</Link>  
+          <Link className="nav-link nav-item" to="/category">Categories</Link>
+        </div>
+      </div>
+    </nav>
   )
 }
