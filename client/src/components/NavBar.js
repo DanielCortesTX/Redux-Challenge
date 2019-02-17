22946fdc618d2f510d994140d8fd6b5c7a92d302
Daniel Cortes <d5t5c5@yahoo.com>
2019-02-16 22:47:53 -0600

    feat: Added styling to Navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 
 export default function navbar() {
   return (
-    <nav>
+    <nav className="navbar navbar-expand-sm p-3">
       <div className="container">
-        <div className="navbar-nav">
-          <Link className="navbar-brand" to="/">Home</Link>  
+        <div className="navbar-nav nav-tabs">
+          <Link className="nav-link nav-item" to="/">Home</Link>  
           <Link className="nav-link nav-item" to="/category">Categories</Link>
         </div>
       </div>
