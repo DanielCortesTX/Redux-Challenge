286a19d93dbbce63aadc764b4bdf5daddd75db30
Daniel Cortes <d5t5c5@yahoo.com>
2019-02-18 20:47:55 -0600

    feat: Created posts reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'thunk'
 import categoriesReducer from './categories'
+import postsReducer from './posts'
 
 export default combineReducers({
-  categort: categoriesReducer
+  category: categoriesReducer,
+  post: postsReducer
 })
---
+++ client/src/reducers/posts.js
@@ -0,0 +1,12 @@
+const initialState = {
+  allPosts: [],
+  activePost: {},
+  loading: false
+}
+
+export default function postsReducer (state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
