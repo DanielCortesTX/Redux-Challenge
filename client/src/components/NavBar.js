2b9357e7329654aedb04c6acf3d7c8d6278ca349
Daniel Cortes <d5t5c5@yahoo.com>
2019-02-20 15:57:45 -0600

    feat: Made conditional nav-link based on active category

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-export default function navbar() {
-  return (
-    <nav className="navbar navbar-expand-sm p-3">
-      <div className="container">
-        <div className="navbar-nav nav-tabs">
-          <Link className="nav-link nav-item" to="/">Home</Link>  
+class NavBar extends Component {
+  render() {
+    const { activeCategory } = this.props
+    // const categoryLink = <Link className="nav-link nav-item" to=`/`></Link>
+    return (
+      <nav className="navbar navbar-expand-sm p-3">
+        <div className="container">
+          <div className="navbar-nav nav-tabs">
+            <Link className="nav-link nav-item" to="/">Home</Link>
+            {activeCategory  && <Link className="nav-link nav-item" to={`/category/${activeCategory}`}>{activeCategory} page</Link>}
+          </div>
         </div>
-      </div>
-    </nav>
-  )
+      </nav>
+    )
+  }
 }
+
+const mapStateToProps = ({ category }) => {
+  return {
+    activeCategory: category.activeCategory
+  }
+}
+
+export default connect(mapStateToProps)(NavBar)
