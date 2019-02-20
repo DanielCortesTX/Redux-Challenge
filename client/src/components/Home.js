0b071130d236b75e88223ec2b1f955a9b33fb5ec
Daniel Cortes <d5t5c5@yahoo.com>
2019-02-19 18:35:29 -0600

    feat: Added getAllPosts functionlity to Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,34 +2,51 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import { getAllCategories } from '../actions/categories'
+import { getAllPosts } from '../actions/posts'
 
 class Home extends Component {
   componentDidMount(){
     this.props.getAllCategories()
+    this.props.getAllPosts()
   }
   render() {
-    const { categories, categoriesLoading } = this.props
+    const { categories, categoriesLoading, postsLoading, posts } = this.props
     let categoryFeed
+    let postFeed
 
     if(categories === null || categoriesLoading){
       categoryFeed = <h1 className="display-4">Loading</h1>
     } else {
       categoryFeed = categories.map((category, index) => <h3 key={index}>{category.name}</h3>)
     }
+
+    if(posts === null || postsLoading){
+      postFeed = <h1 className="display-4">Loading</h1>
+    } else {
+      postFeed = posts.map((post, index) => <h3 key={index}>{post.title}</h3>)
+    }
     return (
-      <div>
+      <div className="container">
         <h1>Home page</h1>
+        <h2>Categories</h2>
+        <hr/>
         {categoryFeed}
+        <hr/>
+        <h2>Posts</h2>
+        <hr/>
+        {postFeed}
       </div>
     )
   }
 }
 
-const mapStateToProps = ({category}) => {
+const mapStateToProps = ({category, post}) => {
   return {
     categories: category.allCategories,
-    categoriesLoading: category.loading
+    categoriesLoading: category.loading,
+    posts: post.allPosts,
+    postsLoading: post.loading
   }
 }
 
-export default connect(mapStateToProps, { getAllCategories })(Home)
+export default connect(mapStateToProps, { getAllCategories, getAllPosts })(Home)
