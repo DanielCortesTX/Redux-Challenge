878685660fc434cb3247296a38bac77c6f9ea9d5
Daniel Cortes <d5t5c5@yahoo.com>
2019-02-21 21:06:46 -0600

    feat: Styled the card on PostDisplay

--- client/src/components/PostDisplay.js
+++ client/src/components/PostDisplay.js
@@ -10,11 +10,12 @@ export default class PostDisplay extends Component {
         <div className="d-flex align-items-center flex-column">
           <h1 className="display-4">{post.title}</h1>
           <hr className="focus-post mb-4"/>
-          <div className="card focus-post d-flex">
+          <div className="card focus-post d-flex p-4">
           <p className="lead">{post.text}</p>
-          <p className="lead">By {post.author}</p>
-          <Moment format="MMMM Do YYYY">{post.date}</Moment>
-          <p>{post.voteScore}</p>
+          <div className="d-flex flex-row">
+            <span className="lead mr-1">Made on <Moment format="MMMM Do YYYY">{post.date}</Moment></span>
+            <p className="lead">By {post.author}</p>
+          </div>
           <div className="d-flex flex-row align-items-center">
             <button className="btn btn-light"><i className="fas fa-angle-up text-info"/></button>
             <button className="btn btn-light"><i className="fas fa-angle-down text-info"/></button>
