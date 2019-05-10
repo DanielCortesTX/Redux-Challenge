0198be1e33c0d4b7e214a4dd8e7b683f81e0766a
Daniel Cortes <d5t5c5@yahoo.com>
2019-05-10 12:32:25 -0500

    feat: Final tweaks for push

---
+++ client/build/precache-manifest.6fd03b62576b09aeba51d15663a90ff6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "309a8f277211d93b67ee",
+    "url": "/static/js/main.309a8f27.chunk.js"
+  },
+  {
+    "revision": "08e485b5d7f330eb346a",
+    "url": "/static/js/2.08e485b5.chunk.js"
+  },
+  {
+    "revision": "309a8f277211d93b67ee",
+    "url": "/static/css/main.0ca86e78.chunk.css"
+  },
+  {
+    "revision": "0c4f4623cc85e89dfe7a445822f47470",
+    "url": "/index.html"
+  }
+];
